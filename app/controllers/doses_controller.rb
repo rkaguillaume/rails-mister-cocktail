1d7ec2bcf1f17def43d9479798c0640025805d91
Guillaume Roche <guillaumeroche24@gmail.com>
2019-02-15 12:13:28 +0100

    cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.save
+
+    redirect_to cocktail_path(params[:cocktail_id])
+  end
+
+  def destroy
+    @dose = Dose.find(params[:cocktail_id])
+    @dose.destroy
+
+    redirect_to cocktail_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
